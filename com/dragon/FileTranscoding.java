3d68006fbabc6a01c199533b65bdac28745aad6d
Tron <asfedasov@gmail.com>
2020-09-13 05:05:35 +0300

    FT - update 1.0

---
+++ com/dragon/FileTranscoding.java
@@ -0,0 +1,47 @@
+package com.dragon;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileTranscoding {
+
+    private static void copyFiles() {
+
+        String string = "Пример текста в кодировке!";
+        Charset iso88591 = StandardCharsets.ISO_8859_1;
+        Charset utf8 = StandardCharsets.UTF_8;
+
+        try {
+            Files.createDirectories(Paths.get("files/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            File newFile = new File("files/newFile.txt");
+            FileWriter fw = new FileWriter(newFile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(string);
+            bw.close();
+
+            File copiedFile = new File("files/copiedFile.txt");
+
+            FileReader reader = new FileReader(newFile, iso88591);
+            FileWriter writer = new FileWriter(copiedFile, utf8);
+
+            reader.transferTo(writer);
+            reader.close();
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        copyFiles();
+    }
+}
